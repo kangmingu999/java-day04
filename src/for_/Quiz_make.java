756052af94d392d2330facd77fed59b4c43469bd
rkdehdrn <rkdehdrn9@naver.com>
2023-06-15 16:10:46 +0900

    w

---
+++ src/for_/Quiz_make.java
@@ -0,0 +1,15 @@
+package for_;
+
+public class Quiz_make {
+public static void main(String[] args) {
+	int a,b,num;
+	
+	for(a=0;a<10;a++) {
+		for(b=1;b<10;b++) {
+			num = b*a;
+		 	System.out.println(a +"*"+ b +"=" +num+"\t");
+		}
+		
+	}
+}
+}
--- src/for_/Testclass05.java
+++ src/for_/Testclass05.java
@@ -2,8 +2,14 @@ package for_;
 
 public class Testclass05 {
 public static void main(String[] args) {
+	/*
 	for( ; ; ) {
 		System.out.println("test");
+	}*/
+	int i = 0;
+	for( ; i<5 ; i++) {
+		System.out.println("i : "+i);
+		i++;
 	}
 }
 }
